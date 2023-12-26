b4c916c73cb0297642e9309db6c0da41814bc8d9
humbl3LilaS <sabishinekobebe@gmail.com>
2023-12-26 23:40:57 +0630

    Tip Calculation Method Change Ver2

--- js/app.js
+++ js/app.js
@@ -8,88 +8,86 @@ let totalPerson;
 let tipPercentage;
 const isBillCorrect = (val) => billRegEx.test(val);
 
-
 billInput.addEventListener("input", () => {
   const waringBill = document.getElementById("warningBill");
   if (!isBillCorrect(billInput.value)) {
-	waringBill.classList.remove("hidden");
-	billInput.parentElement.classList.add("border-red-500");
-	
-	totalBill = 0;
+    waringBill.classList.remove("hidden");
+    billInput.parentElement.classList.add("border-red-500");
+
+    totalBill = 0;
   } else {
-	waringBill.classList.add("hidden");
-	billInput.parentElement.classList.remove("border-red-500");
-	totalBill = Number.parseFloat(billInput.value);
-	resetBtn.disabled = !revealButton();
+    waringBill.classList.add("hidden");
+    billInput.parentElement.classList.remove("border-red-500");
+    totalBill = Number.parseFloat(billInput.value);
+    resetBtn.disabled = !revealButton();
   }
-  
+
   if (billInput.value === "") {
-	totalBill = 0;
-	setTimeout(() => {
-	  billInput.parentElement.classList.remove("border-red-500");
-	  waringBill.classList.add("hidden");
-	}, 1500);
+    totalBill = 0;
+    setTimeout(() => {
+      billInput.parentElement.classList.remove("border-red-500");
+      waringBill.classList.add("hidden");
+    }, 1500);
   }
 });
 
 personInput.addEventListener("input", () => {
   const warningPerson = document.getElementById("warningPerson");
   const num = Number.parseInt(personInput.value);
-  if (num === 0 || !(personRegEx.test(personInput.value))) {
-	totalPerson = 0;
-	warningPerson.classList.remove("hidden");
-	personInput.parentElement.classList.add("border-red-500");
-	if (num === 0) {
-	  warningPerson.innerText = "Can't be zero";
-	} else {
-	  warningPerson.innerText = "Please enter number";
-	}
+  if (num === 0 || !personRegEx.test(personInput.value)) {
+    totalPerson = 0;
+    warningPerson.classList.remove("hidden");
+    personInput.parentElement.classList.add("border-red-500");
+    if (num === 0) {
+      warningPerson.innerText = "Can't be zero";
+    } else {
+      warningPerson.innerText = "Please enter number";
+    }
   } else {
-	warningPerson.classList.add("hidden");
-	personInput.parentElement.classList.remove("border-red-500");
-	totalPerson = num;
-	resetBtn.disabled = !revealButton();
-	calculate();
+    warningPerson.classList.add("hidden");
+    personInput.parentElement.classList.remove("border-red-500");
+    totalPerson = num;
+    resetBtn.disabled = !revealButton();
+    calculate();
   }
-  
+
   if (personInput.value === "") {
-	totalPerson = 0;
-	setTimeout(() => {
-	  personInput.parentElement.classList.remove("border-red-500");
-	  warningPerson.classList.add("hidden");
-	}, 1500);
+    totalPerson = 0;
+    setTimeout(() => {
+      personInput.parentElement.classList.remove("border-red-500");
+      warningPerson.classList.add("hidden");
+    }, 1500);
   }
 });
 
 const optionList = Array.from(document.getElementsByName("tip-amount"));
 
-
-optionList.forEach(ele => ele.addEventListener("click", () => {
-  tipPercentage = +ele.value;
-  resetBtn.disabled = !revealButton();
-  calculate();
-}));
-
+optionList.forEach((ele) =>
+  ele.addEventListener("click", () => {
+    tipPercentage = +ele.value;
+    resetBtn.disabled = !revealButton();
+    calculate();
+  })
+);
 
 const customTip = document.getElementById("custom-tip");
 
 customTip.addEventListener("input", () => {
   if (!billRegEx.test(customTip.value)) {
-	customTip.classList.add("focus:border-red-500");
+    customTip.classList.add("focus:border-red-500");
   } else {
-	customTip.classList.remove("focus:border-red-500");
-	if (+customTip.value > 100) {
-	  tipPercentage = 0;
-	} else {
-	  tipPercentage = Number.parseFloat(customTip.value);
-	}
-	calculate();
-	resetBtn.disabled = !revealButton();
-	calculate();
+    customTip.classList.remove("focus:border-red-500");
+    if (+customTip.value > 100) {
+      tipPercentage = 0;
+    } else {
+      tipPercentage = Number.parseFloat(customTip.value);
+    }
+    calculate();
+    resetBtn.disabled = !revealButton();
+    calculate();
   }
-  if (tipPercentage) optionList.forEach(ele => ele.checked = false);
+  if (tipPercentage) optionList.forEach((ele) => (ele.checked = false));
 });
-
 
 resetBtn.addEventListener("click", () => {
   const tipPerPerson = document.getElementById("tipPerPerson");
@@ -103,7 +101,7 @@ resetBtn.addEventListener("click", () => {
   totalPerson = 0;
   tipPercentage = 0;
   resetBtn.disabled = true;
-  optionList.forEach(ele => ele.checked = false);
+  optionList.forEach((ele) => (ele.checked = false));
 });
 
 function revealButton() {
@@ -112,14 +110,13 @@ function revealButton() {
 
 function calculate() {
   if (!resetBtn.disabled) {
-	const tipPerPerson = document.getElementById("tipPerPerson");
-	const totalTip = document.getElementById("totalAmount");
-	
-	let totalTipAmount = totalBill * (tipPercentage / 100);
-	let tipPerPersonAmount = (totalTipAmount / totalPerson) + (totalBill / totalPerson);
-	
-	tipPerPerson.innerText = "" + tipPerPersonAmount.toFixed(2);
-	totalTip.innerText = "" + totalTipAmount.toFixed(2);
+    const tipPerPerson = document.getElementById("tipPerPerson");
+    const totalTip = document.getElementById("totalAmount");
+
+    let totalTipAmount = totalBill * (tipPercentage / 100);
+    let tipPerPersonAmount = totalTipAmount / totalPerson;
+    let totalBillPerPerson = tipPerPersonAmount + totalBill / totalPerson;
+    tipPerPerson.innerText = "" + tipPerPersonAmount.toFixed(2);
+    totalTip.innerText = "" + totalBillPerPerson.toFixed(2);
   }
 }
-
